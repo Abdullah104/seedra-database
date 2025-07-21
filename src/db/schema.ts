624d6d992fd5b41879c4800cfb740c653d3f6455
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-07-21 13:21:36 +0300

    add form submission id, clientType, clientName, clientFileNumber, clientInvestmentAccountNumber, accountCurrency and portfolioType

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,26 @@
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const adminsTable = pgTable("admins", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
   email: text("email").notNull(),
   password: text("password").notNull(),
+});
+
+export const clientTypeEnum = pgEnum("client_type", ["corporate"]);
+
+export const portfolioTypeEnum = pgEnum("portfolio_type", [
+  "local_securities",
+  "foreign_securities",
+  "investment_funds",
+]);
+
+export const formSubmissionsTable = pgTable("form_submissions", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  clientType: clientTypeEnum(),
+  clientName: text("client_name"),
+  clientFileNumber: text("client_file_number"),
+  clientInvestmentAccountNumber: text("client_investment_account_number"),
+  accountCurrency: text("account_currency"),
+  portfolioType: portfolioTypeEnum(),
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 
-export { adminsTable } from "./db/schema";
+export * from "./db/schema";
 export * from "drizzle-orm/postgres-js";
 export * from "drizzle-orm";
 
-export const db = drizzle('postgresql://postgres.qgwlzjsgaptbklecyseh:iEKtyXn7vZrXSc70@aws-0-eu-north-1.pooler.supabase.com:6543/postgres?pgbouncer=true');
+export const db = drizzle(
+  "postgresql://postgres.qgwlzjsgaptbklecyseh:iEKtyXn7vZrXSc70@aws-0-eu-north-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
+);
